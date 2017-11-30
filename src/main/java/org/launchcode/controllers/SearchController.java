f8cc42816bbc341bd7dd750d025485adc967c5e1
Lucas Ramsey <lucas.r.ramsey@gmail.com>
2017-11-30 12:15:10 -0600

    1st commit, search function works, wrong output format

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,14 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.swing.text.html.FormSubmitEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static javax.swing.text.html.FormSubmitEvent.MethodType.POST;
 
 /**
  * Created by LaunchCode
@@ -23,5 +27,26 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+
+        /*model.addAttribute("title", "Jobs With" + ListController.columnChoices.get(searchTerm) + "Values");
+        model.addAttribute("jobs", jobs);*/
+
+        if (searchType.equals("all") || searchTerm.equals("")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "All " + ListController.columnChoices.get(searchType) + " Values");
+            model.addAttribute("searchTerm", searchTerm);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
+
+    }
 
 }
